124bd761a90819b51583a34bcdd7c13dd8b8f44a
Azamat Uzbekov <batukasss@gmail.com>
2022-03-29 22:13:53 -0600

    def up and down directions created

--- lift.py
+++ lift.py
@@ -5,3 +5,16 @@ import random
 floor_number = random.randint(0, 20)
 passenger_number = random.randint(0, 7)
 print(floor_number, passenger_number)
+
+def up_button(floor_number):
+    for n in range(floor_number):
+        print(n+1)
+
+def down_button(floor_number):
+    for n in range(floor_number, 0, -1):
+        print(n)
+        
+print('Up')       
+up_button(floor_number)
+print('Down')
+down_button(floor_number)
